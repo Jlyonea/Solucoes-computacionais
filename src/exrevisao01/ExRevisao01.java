69ba3ed4215e9e3608e8c69b931389cef526808e
sorieguenight <assasinomortal321321@gmail.com>
2022-04-09 19:19:00 -0300

    ExercicioAreasQuadradoERetangulo

---
+++ src/exrevisao01/ExRevisao01.java
@@ -0,0 +1,12 @@
+package exrevisao01;
+
+import static exrevisao01.Metodos.area;
+
+public class ExRevisao01 {
+
+
+    public static void main(String[] args) {
+       area(2,3);
+    }
+    
+}
---
+++ src/exrevisao01/Metodos.java
@@ -0,0 +1,28 @@
+
+package exrevisao01;
+
+/*
+elabore um projeto java que calcule a area de um quadrado e de um retangulo
+*/
+
+public class Metodos {
+    
+    /**
+     * calcula a area do quadrado
+     * @param lado double
+     * @return 
+     */
+    public static double area(double lado){
+        return lado * lado;
+    }
+    
+    /**
+     * retorna area do retangulo
+     * @param lado double
+     * @param altura double
+     * @return 
+     */
+    public static double area(double lado, double altura){
+        return lado * altura;
+    }
+}
